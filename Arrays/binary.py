ffd7fddae67cb059b8b097b419de4086d9f9bcf6
priyanshuu-dev <p7061815411@gmail.com>
2026-06-28 21:01:37 +0530

    Add files via upload

---
+++ Arrays/binary.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def search(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            guess = (low + high) // 2
+
+            if nums[guess] == target:
+                return guess
+            elif nums[guess] < target:
+                low = guess + 1
+            else:
+                high = guess - 1
+
+        return -1
