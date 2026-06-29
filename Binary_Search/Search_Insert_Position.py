bc14bd05cfdc7484c2fde17fa4e358a743d1515f
priyanshuu-dev <p7061815411@gmail.com>
2026-06-29 19:07:07 +0530

    Add files via upload

---
+++ Binary_Search/Search_Insert_Position.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        low=0
+        high =len(nums)-1
+        result=-1
+
+        while low<=high:
+            mid=(low+high)//2
+
+            if nums[mid]<target:
+                low=mid+1
+            else:
+                result=mid
+                high=mid-1
+
+        return low
+        
+
+ 
