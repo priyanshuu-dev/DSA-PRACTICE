74939258441bd858f7fe9e402fcd961a8c0a7d76
priyanshuu-ml <p7061815411@gmail.com>
2026-08-01 20:31:51 +0530

    leetcode 1209

---
+++ Stack/remove_adj_duplicates_ii.py
@@ -0,0 +1,21 @@
+#leetcode 1209
+
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack=[]
+
+        for i in range(len(s)):
+
+            if stack and stack[-1][0]==s[i]:
+                stack[-1][1]+=1
+
+                if stack[-1][1]==k:
+                    stack.pop()
+            else:
+                stack.append([s[i],1])
+
+        ans=""
+
+        for ch,count in stack:
+            ans+=ch*count
+        return ans
